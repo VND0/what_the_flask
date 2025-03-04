3427234c5cf627571140ca418137bd24ef344c6c
nikita <vnd-08@yandex.ru>
2025-03-04 22:37:00 +0300

    Added the list of the occupations

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
+from typing import Literal
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+OCCUPATIONS = [
+    "инженер-исследователь",
+    "пилот",
+    "строитель",
+    "экзобиолог",
+    "врач",
+    "инженер о терраформированию",
+    "климатолог",
+    "специалист по радиационной защите",
+    "астрогеолог",
+    "гляциолог",
+    "инженер жизнеобеспечения",
+    "метеоролог",
+    "оператор марсохода",
+    "киберинженер",
+    "штурман",
+    "пилот дронов",
+]
 
 
 @app.get("/<title>")
 @app.get("/index/<title>")
 def index(title: str):
     return render_template("base.html", title=title)
+
+
+@app.get("/list_prof/<lst>")
+def list_prof(lst: Literal["ul", "ol"]):
+    return render_template("occup_list.html", lst=lst, occupations=OCCUPATIONS)
 
 
 if __name__ == '__main__':
