bf4a24ca735f14280ab1256322b4a88de49723ae
nikita <vnd-08@yandex.ru>
2025-03-05 14:12:21 +0300

    Added auto_answer route

--- app.py
+++ app.py
@@ -34,5 +34,21 @@ def list_prof(lst: Literal["ul", "ol"]):
     return render_template("occup_list.html", lst=lst, occupations=OCCUPATIONS)
 
 
+@app.get("/answer")
+@app.get("/auto_answer")
+def auto_answer():
+    params = {
+        "title": "Анкета",
+        "surname": "Watny",
+        "name": "Mark",
+        "education": "выше среднего",
+        "profession": "штурман марсохода",
+        "sex": "male",
+        "motivation": "Всегда мечтал застрять на Марсе!",
+        "ready": True,
+    }
+    return render_template("auto_answer.html", **params)
+
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
