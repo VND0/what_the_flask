b8f0aee38bdba57f3071c2aed75b70e3633ebc20
nikita <vnd-08@yandex.ru>
2025-03-08 14:42:28 +0300

    Added rooms distribution

--- app.py
+++ app.py
@@ -76,5 +76,18 @@ def training(prof: str):
     return render_template("training.html", title=title, img=img)
 
 
+@app.get("/distribution")
+def distribution():
+    people = [
+        "Ридли Скотт",
+        "Энди Уир",
+        "Марк Уотни",
+        "Венката Капур",
+        "Тедди Сандерс",
+        "Шон Бин",
+    ]
+    return render_template("distribution.html", title="Размещение по каютам", room_name="Каюта №", people=people)
+
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
